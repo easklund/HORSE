6afb0bec3ead6afde6f1f25b4429ac8921b3aba7
Vilmos Czeroczky <v.czeroczky@gmail.com>
2019-03-07 18:03:45 +0000

    testing with arduino

--- lofi-pi/sensortest.py
+++ lofi-pi/sensortest.py
@@ -1,15 +1,16 @@
 import time
 from sensorreader import SensorReader
+from arduinoreader import ArduinoReader
 
 if __name__ == "__main__":
-	sensor = SensorReader()
+	sensor = ArduinoReader()
 	sensor.start()
 	try:
-		while not sensor.isCalibrated():
-			time.sleep(1)
+##		while not sensor.isCalibrated():
+##			time.sleep(1)
 		while True:
 			print(sensor.getReading())
-			time.sleep(0.05)
+			time.sleep(2)
 	except KeyboardInterrupt:
 		sensor.stop()
 
